d74bbb823be81c0000d759358bef87e37db5296e
Amy Check <amycheck8715@gmail.com>
2020-12-01 10:58:46 -0500

    works before scss is added, navbar is added WIP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+// import './App.scss';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -14,9 +17,7 @@ class App extends React.Component {
   componentDidMount() {
     this.loadMovies();
   }
-  loadImage () {
-    const imgurl = "https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=b6fbc7f3f313bd395902af464ef47262&language=en-US"
-  }
+  
   loadMovies() {
     // go grab (aka get) all the data from some url
     const url = "https://api.themoviedb.org/3/discover/movie?api_key=b6fbc7f3f313bd395902af464ef47262&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1append_to_response=videos,images&include_image_language";
@@ -37,15 +38,29 @@ class App extends React.Component {
     // }
 
     return (
+      
       <div className="App">
-        <h1>Check out these movies!!!!!!</h1>
-        <div className="container">
+        <Navbar bg="light" expand="lg" fixed="top">
+          <Navbar.Brand href="#home"><img src='\assets\images\totallynotnetflix.png'></img></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+        
+        <h1><marquee direction="left" scrollamount="20" behaviour="scroll" className="banner">Check out these movies!!!!!!</marquee></h1>
+        <div className="containmovie">
         {this.state.movies.map(movie =>
           {
           return(
               <div className="image">
                 <img src={'https://image.tmdb.org/t/p/w500'+ movie.poster_path} alt={'poster image for ' + movie.title} className="posterimage" />
-                  <div className="prettify posterimage"><span>{movie.title} ({movie.release_date})</span></div>
+                <div className="prettify posterimage"><span>{movie.title} ({movie.release_date})</span></div>
               </div>
             )
           })}
